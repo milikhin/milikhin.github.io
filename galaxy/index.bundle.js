1de8f8e271c58bffa38462b750e24247b0032919
Mikhail Milikhin <mikhail.milikhin@rubius.com>
2021-09-18 21:56:39 +0200

    add galaxy

---
+++ galaxy/index.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Hello World')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsMEIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9ob3VzZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnSGVsbG8gV29ybGQnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
